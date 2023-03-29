7ce22c9d504e70a35ddf3496c511c25e6c6082b3
marieBvr <lefebvre.m2@gmail.com>
2023-03-29 16:41:48 +0200

    Remove dependency of perl and db

--- tools/blast2ecsv.py
+++ tools/blast2ecsv.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 """
-# Name: blast2ecsv
-# Author: Marie Lefebvre - INRAE
-# Aims: Convert blast xml output to csv and add taxonomy
+# Name: blast2tsv
+# Author(s): Sebastien Theil, Marie Lefebvre - INRAE
+# Aims: Convert blast xml output to tsv and add taxonomy
 #
 """
 import argparse
 import logging as log
 from Bio.Blast import NCBIXML
 from Bio import SeqIO
-import taxoniq, csv
+from Bio import Entrez
+from ete3 import NCBITaxa
+import subprocess
+import time
+
+log.info("Init NCBI taxa database")
+ncbi = NCBITaxa()
 
 def main ():
 	options = _set_options()
@@ -17,11 +23,26 @@ def main ():
 	hits = _read_xml(options)
 	_write_csv(options, hits)
 
+
+def _guess_database(accession):
+    """
+    Guess the correct database for querying based off the format of the accession
+    """
+    database_mappings_refseq = {'AC': 'nuccore', 'NC': 'nuccore', 'NG': 'nuccore',
+        'NT': 'nuccore', 'NW': 'nuccore', 'NZ': 'nuccore',
+        'AP': 'protein', 'NP': 'protein', 'YP': 'protein',
+        'XP': 'protein', 'WP': 'protein', 'EF': 'nuccore',
+        'OK': 'nuccore', 'AF': 'nuccore', 'LC': 'nuccore',
+        'MH': 'nuccore', 'KC': 'nuccore'}
+    return database_mappings_refseq[accession[0:2]]
+
+
 def _read_xml(options):
 	"""
 	Parse XML blast results file
 	Keep only the first hit
 	"""
+	log.info("Read XML file.")
 	results = open(options.xml_file, 'r' )
 	records = NCBIXML.parse(results)
 	xml_results = {}
@@ -31,11 +52,11 @@ def _read_xml(options):
 			for hit in aln.hsps:
 				hsp = {}
 				if hit_count == 1:
-					first_hit_frame = hit.frame[1] # strand
-					cumul_hit_identity = hit.identities
+					first_hit_frame = hit.frame[1] if len(hit.frame) > 0 else 0 # strand
+					cumul_hit_identity = hit.identities if hit.identities else 0
 					cumul_hit_score = hit.bits # hit score
 					cumul_hit_evalue = hit.expect # evalue
-					cumul_hit_length = hit.align_length
+					cumul_hit_length = hit.align_length if hit.align_length != None else 0
 					hit_count = hit_count + 1
 				else:
 					# all HSPs in different strand than 1st HSPs will be discarded.
@@ -57,18 +78,45 @@ def _read_xml(options):
 			hsp["hit_length"] = aln.length # length of the hit
 			hsp["hsp_length"] = hit.align_length # length of the hsp alignment
 			hsp["queryOverlap"] = _get_overlap_value(options.algo, hsp, 'hsp', hsp["query_length"])[0]
-			hsp["percentIdentity"] = round(cumul_hit_identity/cumul_hit_length*100, 1) # identity percentage
+			if cumul_hit_length == 0:
+				hsp["percentIdentity"] = round(cumul_hit_identity, 1) # identity percentage
+			else:
+				hsp["percentIdentity"] = round(cumul_hit_identity/cumul_hit_length*100, 1) # identity percentage
 			hsp["score"] = cumul_hit_score # The higher the bit-score, the better the sequence similarity
 			hsp["num_hsps"] = final_hit_count
 			hsp["hit_cumul_length"] = cumul_hit_length
 			hsp["hitOverlap"] = _get_overlap_value(options.algo, hsp, 'hit', hsp["query_length"])[1]
+			if hsp["accession"].isdigit():
+				strange_acc = hsp["description"].split("|")
+				try:
+					db = _guess_database(strange_acc[1])
+				except KeyError:
+					continue
+			else:
+				db = _guess_database(hsp["accession"])
 			try:
-				t = taxoniq.Taxon(accession_id=hsp["accession"])
-			except KeyError:
-				print("Taxid not found for ", hsp["accession"])
-			hsp["tax_id"] = t.tax_id
-			hsp["taxonomy"] = ';'.join([t.scientific_name for t in reversed(t.ranked_lineage)])
-			hsp["organism"] = t.scientific_name
+				handle = Entrez.esummary(db=db, id=hsp["accession"])
+				taxid = str(int(Entrez.read(handle)[0]['TaxId']))
+				handle.close()
+				log.info("Taxid found for " + hsp["accession"])
+				try:
+					lineage = ncbi.get_lineage(taxid)
+					names = ncbi.get_taxid_translator(lineage)
+					ordered = [names[tid] for tid in lineage]
+					taxonomy = ordered[1:]
+					hsp["tax_id"] = taxid
+					hsp["taxonomy"] = ';'.join(taxonomy)
+					hsp["organism"] = taxonomy[-1]
+				except ValueError:
+					hsp["tax_id"] = ""
+					hsp["taxonomy"] = ""
+					hsp["organism"] = ""
+					log.warning("ValueError - Taxid not found for " + hsp["accession"])
+			except RuntimeError:
+				hsp["tax_id"] = ""
+				hsp["taxonomy"] = ""
+				hsp["organism"] = ""
+				log.warning("RuntimeError - Taxid not found for " + hsp["accession"])
 			if hsp["evalue"] <= options.max_evalue and hsp["queryOverlap"] >= options.min_qov and \
 			hsp["hitOverlap"] >= options.min_hov and hsp["score"] >= options.min_score:
 				xml_results[hsp["query_id"]] = hsp
@@ -101,9 +149,6 @@ def _get_overlap_value(algo, hsp, type, qlength):
 		if hsp["hit_length"]:
 			hit_overlap = (h_align_len*3/hsp["hit_length"])*100
 	elif algo == 'TBLASTX':
-		print(hsp["query_id"])
-		print(q_align_len)
-		print(hsp["hsp_length"])
 		if q_align_len:
 			query_overlap = (q_align_len*3/hsp["hsp_length"])*100
 		if hsp["hit_length"]:
@@ -115,23 +160,32 @@ def _get_overlap_value(algo, hsp, type, qlength):
 			hit_overlap = (h_align_len/hsp["hit_length"])*100
 	if query_overlap == None: query_overlap = 0
 	if query_overlap > 100: query_overlap = 100
-	if hit_overlap == None: hit_overlap = 0
+	if 'hit_overlap' not in locals(): hit_overlap = 0
 	if hit_overlap > 100: hit_overlap = 100
 
 	return [round(query_overlap, 0), round(hit_overlap, 0)]
 
 
 def _write_csv(options, hits):
-	with open(options.rn_file) as rn:
-		rows = ( line.split('\t') for line in rn )
-		rn_list = { row[0]:row[1:] for row in rows }
+	"""
+	Write output
+	"""
+	# get a list of contig without corresponding number of mapped reads
+	if options.rn_file != None:
+		with open(options.rn_file) as rn:
+			rows = ( line.split('\t') for line in rn )
+			rn_list = { row[0]:row[1:] for row in rows }
 	fasta = SeqIO.to_dict(SeqIO.parse(open(options.fasta_file),'fasta'))
 	headers = "#algo\tquery_id\tnb_reads\tquery_length\taccession\tdescription\torganism\tpercentIdentity\tnb_hsps\tqueryOverlap\thitOverlap\tevalue\tscore\ttax_id\ttaxonomy\tsequence\n"
+	log.info("Write output file.")
 	f = open(options.output, "w+")
 	f.write(headers)
 	for h in hits:
-		if len(hits[h]) > 1:
+		if options.rn_file != None:
 			read_nb = ''.join(rn_list[h]).replace("\n", "")
+		else:
+			read_nb = ''
+		if len(hits[h]) > 1:
 			f.write(options.algo + "\t" + h + "\t" + read_nb + "\t" + str(hits[h]["query_length"]) + "\t")
 			f.write(hits[h]["accession"] + "\t" + hits[h]["description"] + "\t")
 			f.write(hits[h]["organism"] + "\t" + str(hits[h]["percentIdentity"]) + "\t")
@@ -141,18 +195,15 @@ def _write_csv(options, hits):
 			f.write(hits[h]["taxonomy"] + "\t" + str(fasta[h].seq))
 			f.write("\n")
 		else:
-			read_nb = ''.join(rn_list[h]).replace("\n", "")
 			f.write(options.algo + "\t" + h + "\t" + read_nb + "\t" + str(hits[h])[1:-1] + "\t")
 			f.write("\n")
 	f.close()
-			
-
 
 
 def _set_options():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-x','--xml', help='XML files with results of blast', action='store', required=True, dest='xml_file')
-	parser.add_argument('-rn','--read-count', help='Tab-delimited file associating seqID with read number.', action='store', required=True, dest='rn_file')
+	parser.add_argument('-rn','--read-count', help='Tab-delimited file associating seqID with read number.', action='store', dest='rn_file')
 	parser.add_argument('-c','--contigs', help='FASTA file with contigs sequence.', action='store', required=True, dest='fasta_file')
 	parser.add_argument('-me' '--max_evalue', help='Max evalue', action='store', type=float, default=0.0001, dest='max_evalue')
 	parser.add_argument('-qov' '--min_query_overlap', help='Minimum query overlap', action='store', type=int, default=5, dest='min_qov')
@@ -164,6 +215,7 @@ def _set_options():
 	args = parser.parse_args()
 	return args
 
+
 def _set_log_level(verbosity):
 	if verbosity == 1:
 		log_format = '%(asctime)s %(levelname)-8s %(message)s'
@@ -174,4 +226,5 @@ def _set_log_level(verbosity):
 
 if __name__ == "__main__":
 	main()
+
 
